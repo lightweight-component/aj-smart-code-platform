bff56541b03bf8597c928558a9b6b1a3da64b988
sp42@qq.com <sp42@qq.com>
2024-07-02 16:28:18 +0800

    ci

--- src/route.ts
+++ src/route.ts
@@ -1,6 +1,5 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-// import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
 
 Vue.use(VueRouter);
 
