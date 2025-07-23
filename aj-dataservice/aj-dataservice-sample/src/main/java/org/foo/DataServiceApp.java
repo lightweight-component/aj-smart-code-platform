9719198ac36214b31290da0b5b8b19ee186e11c9
张鑫 <sp42@qq.com>
2025-07-23 17:55:05 +0800

    整合 IAM

--- aj-dataservice/aj-dataservice-sample/src/main/java/org/foo/DataServiceApp.java
+++ aj-dataservice/aj-dataservice-sample/src/main/java/org/foo/DataServiceApp.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DataServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(DataServiceApp.class, args);
+        System.out.println("\u001B[32m======================================(♥◠‿◠)ﾉﾞ 【AJ-Data Service 启动成功】ლ(´ڡ`ლ)ﾞ==============================================\u001B[0m"); // 绿色文本
     }
 }
---
+++ aj-dataservice/aj-dataservice-sample/src/main/java/org/foo/controller/ClientController.java
@@ -0,0 +1,58 @@
+package org.foo.controller;
+
+import com.ajaxjs.iam.UserConstants;
+import com.ajaxjs.iam.client.BaseOidcClientUserController;
+import com.ajaxjs.iam.jwt.JwtAccessToken;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 用户客户端
+ */
+@RestController
+@RequestMapping("/client")
+public class ClientController extends BaseOidcClientUserController {
+    @Override
+    public JwtAccessToken onAccessTokenGot(JwtAccessToken token, HttpServletResponse resp, HttpSession session) {
+        String tokenStr = token.getId_token();
+        // 设置 Token 到 Cookie
+        ResponseCookie cookie = ResponseCookie.from(UserConstants.ACCESS_TOKEN_KEY, tokenStr)
+                .httpOnly(true)
+                .secure(false) // TODO for prod
+                .path("/")
+                .sameSite("Strict") // 设置 SameSite 属性
+                .maxAge(3600 * 24 * 3) // 有效期 1 小时
+                .build();
+
+        resp.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        return token;
+    }
+
+    @Value("${user.clientId}")
+    private String clientId;
+
+    @Value("${user.clientSecret}")
+    private String clientSecret;
+
+    @GetMapping("/to_login")
+    public RedirectView loginPageUrl(HttpSession session, @RequestParam(required = false) String web_url) {
+        return loginPageUrl(session, "/iam_api/oidc/authorization", clientId, "/iam_api/client/callback", web_url);
+    }
+
+    @GetMapping("/callback")
+    public ModelAndView callbackToken(@RequestParam String code, @RequestParam String state, @RequestParam(required = false) String web_url, HttpSession session, HttpServletResponse resp) {
+        return callbackToken(clientId, clientSecret, code, state, web_url, session, resp);
+    }
+
+}
